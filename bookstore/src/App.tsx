1f9afb2ab7bdd62d627fcf8f4a9a114ed1e7138a
ssoferret <owosso1212@gmail.com>
2024-06-11 16:44:13 +0900

    🚩 ThemeProvider에 토글 적용

--- bookstore/src/App.tsx
+++ bookstore/src/App.tsx
@@ -3,16 +3,16 @@ import Detail from "./pages/Detail";
 import Home from "./pages/Home";
 import { GlobalStyle } from './style/global';
 import { ThemeProvider } from "styled-components";
-import { ThemeName, light, dark} from './style/theme';
+import { ThemeName, light, dark, getTheme} from './style/theme';
 import ThemeSwitcher from "./components/header/ThemeSwitcher";
 import { useState } from "react";
 
 function App() {
   const [themeName, setThemeName] = useState<ThemeName>('light');
   return (
-    <ThemeProvider theme={light}>
-      <GlobalStyle themeName="light"/>
-      <ThemeSwitcher />
+    <ThemeProvider theme={getTheme(themeName)}>
+      <GlobalStyle themeName={themeName}/>
+      <ThemeSwitcher themeName={themeName} setThemeName={setThemeName}/>
       <Layout children={<Home />} />
     </ThemeProvider>
   );
