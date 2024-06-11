b5b4c4146e78631c1f30a2336c598db131593a5f
ssoferret <owosso1212@gmail.com>
2024-06-11 17:25:19 +0900

    ♻️ ThemeSwitcher가 props를 통해서가 아닌 외부에서 context를 읽을 수 있도록 수정

--- bookstore/src/components/header/ThemeSwitcher.tsx
+++ bookstore/src/components/header/ThemeSwitcher.tsx
@@ -1,16 +1,19 @@
+import { useContext } from "react";
 import { ThemeName } from "../../style/theme";
+import { ThemeContext } from "../../context/themeContext";
 
-interface Props {
-    themeName: ThemeName;
-    setThemeName: (themeName: ThemeName) => void;
-}
+// interface Props {
+//     themeName: ThemeName;
+//     setThemeName: (themeName: ThemeName) => void;
+// }
 
-function ThemeSwitcher({themeName, setThemeName} : Props) {
-    const toggleTheme = () => {
-        setThemeName(themeName === 'light' ? 'dark' : 'light');
-    };
+function ThemeSwitcher() {
+    const {themeName, toggleTheme} = useContext(ThemeContext);
+    // const toggleTheme = () => {
+    //     setThemeName(themeName === 'light' ? 'dark' : 'light');
+    // };
     return (
-        <button onClick={toggleTheme}>{ themeName }</button>
+        <button onClick={() => toggleTheme}>{ themeName }</button>
     )
 }
 
