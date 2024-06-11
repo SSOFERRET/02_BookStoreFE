d60098ea09e4b4ce699b17d579e4d5fcfb2e09d1
ssoferret <owosso1212@gmail.com>
2024-06-11 17:08:27 +0900

    🏷️ themeContext에 state 및 type 적용

--- bookstore/src/context/themeContext.tsx
+++ bookstore/src/context/themeContext.tsx
@@ -1,4 +1,14 @@
 import { createContext } from "react";
 import { ThemeName } from "../style/theme";
 
-export const ThemeContext = createContext<ThemeName>('light');
+interface State {
+    themeName: ThemeName;
+    setThemeName: (themeName: ThemeName) => void;
+}
+
+export const state = {
+    themeName: 'light' as ThemeName, 
+    setThemeName: (themeName: ThemeName) => {}
+}
+
+export const ThemeContext = createContext<State>(state);
