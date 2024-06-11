7a81d80bf03a70729f8243733db5736f9b270bdc
ssoferret <owosso1212@gmail.com>
2024-06-11 22:57:38 +0900

    🚩 title  component 추가

--- bookstore/src/pages/Home.tsx
+++ bookstore/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Footer from "../components/common/Footer";
 import Header from "../components/common/Header";
+import Title from "../components/common/Title";
 
 function Home() {
     return (
         <>
+            <Title size='large'>title test</Title>
             <div>home body</div>
         </>
     )
