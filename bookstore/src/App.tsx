6dde70d3c5b48a73321d197d89d75c05b48844b7
ssoferret <owosso1212@gmail.com>
2024-06-11 23:24:10 +0900

    🔥  불필요한 import 삭제

--- bookstore/src/App.tsx
+++ bookstore/src/App.tsx
@@ -1,18 +1,15 @@
 import Layout from "./components/layout/Layout";
-import Detail from "./pages/Detail";
 import Home from "./pages/Home";
-import { GlobalStyle } from './style/global';
-import { ThemeProvider } from "styled-components";
-import { ThemeName, light, dark, getTheme} from './style/theme';
 import ThemeSwitcher from "./components/header/ThemeSwitcher";
-import { useContext, useState } from "react";
-import { BookStoreThemeProvider, ThemeContext } from "./context/themeContext";
+import { BookStoreThemeProvider } from "./context/themeContext";
 
 function App() {
   return (
     <BookStoreThemeProvider>
         <ThemeSwitcher />
-        <Layout children={<Home />} />
+        <Layout>
+          <Home />
+        </Layout>
     </BookStoreThemeProvider>
   );
 }
