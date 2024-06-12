258625c2f7f879c7842e7b339d2dce297d9cb718
ssoferret <owosso1212@gmail.com>
2024-06-12 14:00:17 +0900

    🚩 InputText component 추가

--- bookstore/src/pages/Home.tsx
+++ bookstore/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import Button from "../components/common/Button";
+import InputText from "../components/common/InputText";
 import Title from "../components/common/Title";
 
 function Home() {
@@ -6,6 +7,7 @@ function Home() {
         <>
             <Title size='large'>title test</Title>
             <Button size='large' scheme='primary' disabled={true}> 버튼 테스트</Button>
+            <InputText placeholder="여기에 입력하세요" />
             <div>home body</div>
         </>
     )
