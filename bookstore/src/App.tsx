64883d4e306b014810b95381ff418b1bfc241c86
ssoferret <owosso1212@gmail.com>
2024-06-12 15:54:49 +0900

    🚩 몸통 router 적용

--- bookstore/src/App.tsx
+++ bookstore/src/App.tsx
@@ -2,13 +2,28 @@ import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
 import ThemeSwitcher from "./components/header/ThemeSwitcher";
 import { BookStoreThemeProvider } from "./context/themeContext";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from "./components/common/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <Error />
+  },
+  {
+    path: "/books",
+    element: <div>도서 목록</div>
+  },
+  
+])
 
 function App() {
   return (
     <BookStoreThemeProvider>
         <ThemeSwitcher />
         <Layout>
-          <Home />
+          <RouterProvider router={router} />
         </Layout>
     </BookStoreThemeProvider>
   );
