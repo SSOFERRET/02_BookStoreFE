8439c7101e22fd33f41aaebc0e15221bacd0a1ed
ssoferret <owosso1212@gmail.com>
2024-06-12 17:40:30 +0900

    🐛 cors 연결 위해 수정

--- bookstore/src/api/category.api.ts
+++ bookstore/src/api/category.api.ts
@@ -2,6 +2,13 @@ import { Category } from "../models/category.model";
 import { httpClient } from "./http";
 
 export const fecthCategory = async () => {
-    const response = await httpClient.get<Category[]>('/category');
-    return response.data;
+    const response = await fetch('http://localhost:9999/category', {
+    //httpClient.get<Category[]>('/category');
+        method: "get",
+        headers: {
+            "content-type": "application/json"
+        }
+    })
+    const resJson = await response.json();
+    return resJson;
 }
