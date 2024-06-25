febcb39aef3e9e3a5d74b9f864802e08c8676209
ssoferret <owosso1212@gmail.com>
2024-06-25 15:54:15 +0900

    Test: 개별 도서 조회 컴포넌트 테스트

---
+++ bookstore/src/components/books/BookItem.spec.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { getByAltText, render } from "@testing-library/react";
+import BookItem from "./BookItem";
+import { BookStoreThemeProvider } from "../../context/themeContext";
+
+const dummyBook = {
+    book_id: 1,
+    title: "dummy",
+    img: 5,
+    category_id: 1,
+    form: "paperback",
+    isbn: "Dummy ISBN",
+    summary: "Dummy Summary",
+    detail: "Dummy Detail",
+    author: "Dummy Author",
+    pages: 100,
+    contents: "Dummy Contents",
+    price: 10000,
+    likes: 1,
+    pubDate: "2021-01-01",
+};
+
+describe("BookItem", () => {
+    it("렌더 여부", () => {
+        const { getByText } = render(
+            <BookStoreThemeProvider>
+                <BookItem book={dummyBook} />
+            </BookStoreThemeProvider>
+        );
+        expect(getByText(dummyBook.title)).toBeInTheDocument();
+        expect(getByText(dummyBook.summary)).toBeInTheDocument();
+        expect(getByText(dummyBook.author)).toBeInTheDocument();
+        expect(getByText("10,000원")).toBeInTheDocument();
+        expect(getByText(dummyBook.likes)).toBeInTheDocument();
+    })
+})
