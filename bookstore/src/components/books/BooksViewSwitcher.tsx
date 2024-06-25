1aceca92826aa509e0dac875cfee9ba13dab8921
ssoferret <owosso1212@gmail.com>
2024-06-25 16:51:46 +0900

    Feat: 도서 목록 뷰 전환 버튼 생성 #5

---
+++ bookstore/src/components/books/BooksViewSwitcher.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import Button from "../common/Button";
+import { FaList, FaTh } from "react-icons/fa";
+import { useSearchParams } from "react-router-dom";
+import { QUERYSTRING } from "../constants/querystring";
+
+const viewOptions = [
+    {
+        value: "list",
+        icon: <FaList />
+    },
+    {
+        value: "grid",
+        icon: <FaTh />
+    },
+];
+
+function BooksViewSwitcher() {
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const handleSwitch = (value: string) => {
+        const newSearchParams = new URLSearchParams(searchParams);
+        newSearchParams.set(QUERYSTRING.VIEW, value);
+        setSearchParams(newSearchParams);
+    }
+
+    return (
+        <BooksViewSwitcherStyle>
+            {
+                viewOptions.map((option) => (
+                    <Button size="medium" 
+                    scheme={searchParams.get(QUERYSTRING.VIEW) === option.value ? 
+                        "primary" : "normal"
+                    }
+                    onClick={() => handleSwitch(option.value)}>
+                        {option.icon}
+                    </Button>
+                ))
+            }
+        </BooksViewSwitcherStyle>
+    )
+}
+
+const BooksViewSwitcherStyle = styled.div``;
+
+export default BooksViewSwitcher;
