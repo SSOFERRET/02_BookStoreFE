8256b363add91e937c9db72b3643bbe0b61e0527
ssoferret <owosso1212@gmail.com>
2024-06-25 15:59:42 +0900

    Feat: 책 필터 버튼 선택 여부 표시 위한 작업

--- bookstore/src/hooks/useCategory.ts
+++ bookstore/src/hooks/useCategory.ts
@@ -1,9 +1,36 @@
 import { useEffect, useState } from "react";
 import { fecthCategory } from "../api/category.api";
 import { Category } from "../models/category.model";
+import { useLocation } from "react-router-dom";
 
 export const useCategory = () => {
+    const location = useLocation();
     const [category, setCategory] = useState<Category[]>([]);
+
+    const setActive = () => {
+        const params = new URLSearchParams(location.search);
+        if (params.get('category_id')) {
+            setCategory((prev) => {
+                return prev.map((item) => {
+                    return {
+                        ...item,
+                        isActive: item.category_id === Number(params
+                            .get('category_id')),
+                    };
+                });
+            });
+        } else {
+            setCategory((prev) => {
+                return prev.map((item) => {
+                    return {
+                        ...item,
+                        isActive: false
+                    };
+                });
+            });
+        }
+    }
+
     useEffect(() => {
         fecthCategory().then((category) => {
             if(!category) return;
@@ -15,8 +42,13 @@ export const useCategory = () => {
                 ...category,
             ]
             setCategory(categoryWithAll);
+            setActive();
         })
     }, []);
+
+    useEffect(() => {
+        setActive();
+    }, [location.search]);
 
     return { category };
 }
