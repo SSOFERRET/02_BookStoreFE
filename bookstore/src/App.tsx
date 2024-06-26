d8d746538c8247da8f1769663518adec21e24999
ssoferret <owosso1212@gmail.com>
2024-06-26 11:46:21 +0900

    Feat: 도서 상세 페이지 라우팅 #6

--- bookstore/src/App.tsx
+++ bookstore/src/App.tsx
@@ -8,6 +8,7 @@ import Signup from "./pages/Signup";
 import ResetPassword from "./pages/ResetPassword";
 import Login from "./pages/Login";
 import Books from "./pages/Books";
+import BookDetail from "./pages/BookDetail";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,14 @@ const router = createBrowserRouter([
   {
     path: "/books",
     element: <Layout><Books /></Layout>
+  },
+  {
+    path: "/book/:bookId",
+    element: (
+      <Layout>
+        <BookDetail />
+      </Layout>
+    )
   },
   {
     path: "/signup",
