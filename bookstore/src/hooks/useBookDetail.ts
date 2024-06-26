730b4a884016c0dc7dd40868f9786c6349b0b3ad
ssoferret <owosso1212@gmail.com>
2024-06-26 14:23:16 +0900

    Fix: useBookDetail 매개변수 타입 undefined 추가

--- bookstore/src/hooks/useBookDetail.ts
+++ bookstore/src/hooks/useBookDetail.ts
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import { BookDetail } from "../models/book.model";
 import { fetchBook } from "../api/books.api";
  
-export const useBookDetail = (bookId: string) => {
+export const useBookDetail = (bookId: string | undefined) => {
     const [book, setBook] = useState<BookDetail | null>(null);
 
     useEffect(() => {
+        if (!bookId) return;
+        
         fetchBook(bookId).then((book) => {
             setBook(book);
         })
