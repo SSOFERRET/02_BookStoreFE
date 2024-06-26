a78352347bf2a0e370cdd790d93070d98cf966d4
ssoferret <owosso1212@gmail.com>
2024-06-26 11:45:58 +0900

    Feat: 도서 상세 페이지 파일 생성 #6

---
+++ bookstore/src/pages/BookDetail.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+function BookDetail() {
+    return (
+        <BookDetailStyle>
+            <h1>BookDetail</h1>
+        </BookDetailStyle>
+    )
+}
+
+const BookDetailStyle = styled.div`
+    
+`;
+
+export default BookDetail;
