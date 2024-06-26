c0f904d82d58f3c6eef8cafced89e7132644d7f3
ssoferret <owosso1212@gmail.com>
2024-06-26 15:02:32 +0900

    Style: 도서 상세 페이지 목차 스타일 작성 #6

--- bookstore/src/pages/BookDetail.tsx
+++ bookstore/src/pages/BookDetail.tsx
@@ -124,6 +124,15 @@ const BookDetailStyle = styled.div`
             }
         }
     }
+    .content {
+        .detail {
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 4;
+            -webkit-box-orient: vertical;
+        }
+    }
 `;
 
 export default BookDetail;
