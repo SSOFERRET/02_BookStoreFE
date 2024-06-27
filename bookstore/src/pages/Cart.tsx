70add1a2b51b5e50af9c99a5ee6c5342e81998c3
ssoferret <owosso1212@gmail.com>
2024-06-27 16:36:24 +0900

    [Feat/#9] 장바구니 목록 조회 아이템 컴포넌트 나열

--- bookstore/src/pages/Cart.tsx
+++ bookstore/src/pages/Cart.tsx
@@ -1,13 +1,22 @@
 import styled from "styled-components"
 import Title from "../components/common/Title"
 import CartItem from "../components/cart/CartItem";
+import { useCart } from "../hooks/useCart";
 
 function Cart() {
+    const { carts } = useCart();
+
+    console.log(carts);
+    console.log(typeof carts)
     return (
         <CartStyle>
             <Title size="large">장바구니</Title>
             <div className='content'>
-                <CartItem />
+                {
+                    carts.map((item) => (
+                        <CartItem cart={item} key={item.cart_item_id} />
+                    ))
+                }
             </div>
             <div className="summary">summary</div>
         </CartStyle>
