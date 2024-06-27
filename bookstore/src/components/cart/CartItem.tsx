36157909713a7211b9063c0b1d450305b36751c5
ssoferret <owosso1212@gmail.com>
2024-06-27 16:37:45 +0900

    [Feat/#9] 장바구니 목록 개별 컴포넌트 페이지 렌더 배치

--- bookstore/src/components/cart/CartItem.tsx
+++ bookstore/src/components/cart/CartItem.tsx
@@ -1,9 +1,29 @@
 import styled from "styled-components"
+import { Cart } from "../../models/cart.model";
+import Title from "../common/Title";
+import { formatNumber } from "../../utils/format";
+import Button from "../common/Button";
 
-function CartItem() {
+interface Props {
+    cart: Cart;
+}
+
+function CartItem({ cart }: Props) {
     return (
         <CartItemStyle>
-            <div className="summary">cart item</div>
+            <div className="info">
+                <div>체크 버튼</div>
+                <div>
+                    <Title size="medium">{cart.title}</Title>
+                    <p className="summary">{cart.summary}</p>
+                    <p className="price">{formatNumber(cart.price)}원</p>
+                    <p className="quantity">{cart.quantity}권</p>
+                </div>
+            </div>
+
+            <Button size="medium" scheme="normal">
+                장바구니 삭제
+            </Button>
         </CartItemStyle>
     )
 }
