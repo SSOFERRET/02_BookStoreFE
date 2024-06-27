cf2b7206d8da1290c45a5f880377c39f2e8c8e90
ssoferret <owosso1212@gmail.com>
2024-06-27 17:21:51 +0900

    [Feat/#9] 장바구니 목록 아이템 체크 기능 작성

--- bookstore/src/components/cart/CartItem.tsx
+++ bookstore/src/components/cart/CartItem.tsx
@@ -4,18 +4,30 @@ import Title from "../common/Title";
 import { formatNumber } from "../../utils/format";
 import Button from "../common/Button";
 import CheckIconButton from "../common/CheckIconButton";
+import { useMemo } from "react";
 
 interface Props {
     cart: Cart;
+    checkedItems: number[];
+    onCheck: (id: number) => void;
 }
 
-function CartItem({ cart }: Props) {
+function CartItem({ cart, checkedItems, onCheck }: Props) {
+    // checkedItems 목록에 내가 있나 ? isChecked : !isChecked
+    const isChecked = useMemo(() => {
+        return checkedItems.includes(cart.cart_item_id);
+    }, [checkedItems, cart.cart_item_id]);
+
+    const handleCheck = () => {
+        onCheck(cart.cart_item_id);
+    };
+
     return (
         <CartItemStyle>
             <div className="info">
                 <div>
-                    <CheckIconButton isChecked={true} 
-                    onCheck={() => {}}/>
+                    <CheckIconButton isChecked={isChecked} 
+                    onCheck={handleCheck}/>
                 </div>
                 <div>
                     <Title size="medium">{cart.title}</Title>
--- bookstore/src/pages/Cart.tsx
+++ bookstore/src/pages/Cart.tsx
@@ -2,19 +2,25 @@ import styled from "styled-components"
 import Title from "../components/common/Title"
 import CartItem from "../components/cart/CartItem";
 import { useCart } from "../hooks/useCart";
+import { useState } from "react";
 
 function Cart() {
     const { carts } = useCart();
+    const [ checkedItems, setCheckedItems ] = useState<number[]>([]);
 
-    console.log(carts);
-    console.log(typeof carts)
+    const handleCheckItem = (id: number) => {
+        if (checkedItems.includes(id)) 
+            return setCheckedItems(checkedItems.filter((item) => item !== id));
+        return setCheckedItems([...checkedItems, id])
+    }
+
     return (
         <CartStyle>
             <Title size="large">장바구니</Title>
             <div className='content'>
                 {
                     carts.map((item) => (
-                        <CartItem cart={item} key={item.cart_item_id} />
+                        <CartItem cart={item} key={item.cart_item_id} checkedItems={checkedItems} onCheck={handleCheckItem}/>
                     ))
                 }
             </div>
