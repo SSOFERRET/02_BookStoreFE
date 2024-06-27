7ad4439133b65157f33d917a217e5af7f83d420c
ssoferret <owosso1212@gmail.com>
2024-06-27 13:42:36 +0900

    [Feat/#7] useBookDetail 훅에 likeToggle 함수 추가

--- bookstore/src/hooks/useBookDetail.ts
+++ bookstore/src/hooks/useBookDetail.ts
@@ -1,9 +1,41 @@
 import { useEffect, useState } from "react";
 import { BookDetail } from "../models/book.model";
-import { fetchBook } from "../api/books.api";
+import { fetchBook, likeBook, unLikeBook } from "../api/books.api";
+import { useAuthStore } from "../store/authStore";
+import { useAlert } from "./useAlert";
  
 export const useBookDetail = (bookId: string | undefined) => {
     const [book, setBook] = useState<BookDetail | null>(null);
+    const { isloggedIn } = useAuthStore();
+    const showAlert = useAlert();
+
+    const likeToggle = () => {
+        if (!isloggedIn) {
+            showAlert('로그인이 필요합니다.');
+            return;
+        }
+         
+        if (!book) return;
+
+        if (book.liked) {
+            unLikeBook(book.book_id).then(() => {
+                setBook({
+                    ...book,
+                    liked: false,
+                    likes: book.likes - 1,
+                });
+            });
+        } else {
+            console.log(book.likes);
+            likeBook(book.book_id).then(() => {
+                setBook({
+                    ...book,
+                    liked: true,
+                    likes: book.likes + 1,
+                });
+            });
+        }
+    };
 
     useEffect(() => {
         if (!bookId) return;
@@ -13,5 +45,5 @@ export const useBookDetail = (bookId: string | undefined) => {
         })
     }, [bookId]);
 
-    return { book };
+    return { book, likeToggle };
 }
