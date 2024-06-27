1aad536f4827509a2d2bc29de9d106214d36d281
ssoferret <owosso1212@gmail.com>
2024-06-27 16:41:14 +0900

    [Style/#9] 장바구니 목록 개별 아이템 컴포넌트 스타일링

--- bookstore/src/components/cart/CartItem.tsx
+++ bookstore/src/components/cart/CartItem.tsx
@@ -29,7 +29,17 @@ function CartItem({ cart }: Props) {
 }
 
 const CartItemStyle = styled.div`
-    
+    display: flex;
+    justify-content: space-between;
+    align-items: start;
+    border: 1px solid ${({ theme }) => theme.color.border};
+    border-radius: ${({ theme }) => theme.borderRadius.default};
+    padding: 12px;
+
+    p {
+        padding: 0 0 8px 0;
+        margin: 0;
+    }
 `;
 
 export default CartItem;
