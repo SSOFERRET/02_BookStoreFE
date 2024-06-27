4b299799c09243663b6902f414c5db6f5600be4f
ssoferret <owosso1212@gmail.com>
2024-06-27 14:59:51 +0900

    [Style, Feat/#8] 장바구니 추가 스타일 및 API와의 연동 작업

--- bookstore/src/components/book/AddToCart.tsx
+++ bookstore/src/components/book/AddToCart.tsx
@@ -3,6 +3,10 @@ import Button from "../common/Button";
 import InputText from "../common/InputText";
 import { BookDetail } from "../../models/book.model";
 import { useState } from "react";
+import { addToCart as fetchAddToCart } from "../../api/carts.api";
+import { useAlert } from "../../hooks/useAlert";
+import { Link } from "react-router-dom";
+// import { useAuthStore } from "../../store/authStore";
 
 interface Props {
     book: BookDetail
@@ -10,6 +14,7 @@ interface Props {
 
 function AddToCart({ book }: Props) {
     const [quantity, setQuantity] = useState<number>(1);
+    const [cartAdded, setCartAdded] = useState<boolean>(false);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setQuantity(Number(e.target.value));
@@ -24,8 +29,20 @@ function AddToCart({ book }: Props) {
         setQuantity(quantity - 1);
     }
 
+    const addToCart = () => {
+        fetchAddToCart({
+            book_id: book.book_id,
+            quantity: quantity
+        }).then(() => {
+            setCartAdded(true);
+            setTimeout(() => {
+                setCartAdded(false);
+            }, 3000);
+        })
+    }
+
     return (
-        <AddToCartStyle>
+        <AddToCartStyle $added={cartAdded}>
             <div>
                 <InputText inputType="number" value={quantity}
                 onChange={handleChange} />
@@ -38,17 +55,48 @@ function AddToCart({ book }: Props) {
                     -
                 </Button>
             </div>
-            <Button size="medium" scheme="primary">
+            <Button size="medium" scheme="primary"
+            onClick={addToCart}>
                 장바구니 담기
             </Button>
+            { cartAdded && (
+                    <div className="added">
+                        <p>장바구니에 추가 되었습니다.</p>
+                        <Link to="/cart">장바구니로 이동</Link>
+                    </div>
+                )
+            }
         </AddToCartStyle>
     )
 }
 
-const AddToCartStyle = styled.div`
+interface AddToCartStyleProps {
+    $added: boolean;
+}
+
+const AddToCartStyle = styled.div<AddToCartStyleProps>`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    position: relative;
+
+    .added {
+        position: absolute;
+        right: 0;
+        bottom: -90px;
+        background: ${({ theme }) => theme.color.background};
+        border-radius: ${({ theme }) => theme.borderRadius.default};
+        padding: 8px 12px;
+        opacity: ${({ $added }) => ($added ? "1" : "0")};
+        transition: all 0.5s ease;
+
+        p {
+            padding: 0 0 8px 0;
+            margin: 0;
+        }
+    }
+
+    
 `;
 
 export default AddToCart;
