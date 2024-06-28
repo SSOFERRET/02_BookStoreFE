56b9c049eb759182962e86b7bf653e31ba34a321
ssoferret <owosso1212@gmail.com>
2024-06-28 16:21:42 +0900

    [Fix/#9] Order 페이지 라우팅 중 import 누락

--- bookstore/src/App.tsx
+++ bookstore/src/App.tsx
@@ -10,6 +10,7 @@ import Login from "./pages/Login";
 import Books from "./pages/Books";
 import BookDetail from "./pages/BookDetail";
 import Cart from "./pages/Cart";
+import Order from "./pages/Order";
 
 const router = createBrowserRouter([
   {
