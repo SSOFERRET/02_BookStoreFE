7745efad1f731e0fb53537fa2d8599e827475dc5
ssoferret <owosso1212@gmail.com>
2024-06-28 17:21:53 +0900

    [Design/#12] 주문서 UI

--- bookstore/src/pages/Cart.tsx
+++ bookstore/src/pages/Cart.tsx
@@ -122,6 +122,35 @@ export const CartStyle = styled.div`
         border-radius: ${({ theme }) => theme.borderRadius.default};
         padding: 12px;
     }
+
+    .delivery {
+        fieldset {
+            border: 0;
+            margin: 0;
+            padding: 0 0 12px 0;
+            display: flex;
+            justify-content: start;
+            gap: 8px;
+            
+            label {
+                width: 80px;
+            }
+
+            .input {
+                flex: 1;
+                input {
+                    width: 100%;
+                }
+            }
+        }
+
+        .error-text {
+            color: red;
+            margin: 0;
+            padding: 0 0 12px 0;
+            text-align: right;
+        }
+    }
 `;
 
 export default Cart;
