8eb31dd71fb4d52a9c83ce1a9d8b7937a4085b3d
ssoferret <owosso1212@gmail.com>
2024-06-28 12:54:08 +0900

    [Feat/#11] Empty 컴포넌트 배치

--- bookstore/src/pages/Cart.tsx
+++ bookstore/src/pages/Cart.tsx
@@ -4,9 +4,11 @@ import CartItem from "../components/cart/CartItem";
 import { useCart } from "../hooks/useCart";
 import { useState } from "react";
 import { deleteCart } from "../api/carts.api";
+import Empty from "../components/books/Empty";
+import { FaShoppingCart } from "react-icons/fa";
 
 function Cart() {
-    const { carts, deleteCartItem } = useCart();
+    const { carts, isEmpty, deleteCartItem } = useCart();
     const [ checkedItems, setCheckedItems ] = useState<number[]>([]);
 
     const handleCheckItem = (id: number) => {
@@ -20,22 +22,44 @@ function Cart() {
     }
 
     return (
-        <CartStyle>
-            <Title size="large">장바구니</Title>
-            <div className='content'>
-                {
-                    carts.map((item) => (
-                        <CartItem cart={item} key={item.cart_item_id} checkedItems={checkedItems} onCheck={handleCheckItem} onDelete={handleDeleteItem} />
-                    ))
-                }
-            </div>
-            <div className="summary">summary</div>
-        </CartStyle>
+        <>
+        <Title size="large">장바구니</Title>
+            <CartStyle>
+                {!isEmpty && (<>
+                    <div className='content'>
+                        {
+                            carts.map((item) => (
+                                <CartItem cart={item} key={item.cart_item_id} checkedItems={checkedItems} onCheck={handleCheckItem} onDelete={handleDeleteItem} />
+                            ))
+                        }
+                        </div>
+                        <div className="summary">summary</div>
+                    </>
+                )}
+                {isEmpty && (
+                    <Empty title="장바구니가 비었습니다." icon={<FaShoppingCart />} />
+                )}
+            </CartStyle>
+        </>
     )
 }
 
 const CartStyle = styled.div`
-    
+    display: flex;
+    gap: 24px;
+    justify-content: space-between;
+    padding: 24px 0 0 0;
+
+    .content {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        gap: 12px;
+    }
+
+    .summary {
+        display: flex;
+    }
 `;
 
 export default Cart;
