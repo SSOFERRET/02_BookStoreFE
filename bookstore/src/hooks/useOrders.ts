4157b8debeca956c3062a8d1ab3cc62c67db4968
ssoferret <owosso1212@gmail.com>
2024-06-30 15:45:49 +0900

    [Feat/#13] useOrders에 주문 내역 상세 조회를 위한 selectOrderItem 추가

--- bookstore/src/hooks/useOrders.ts
+++ bookstore/src/hooks/useOrders.ts
@@ -1,15 +1,41 @@
 import { useEffect, useState } from "react"
-import { Order } from "../models/order.model";
-import { fetchOrders } from "../api/order.api";
+import { Order, OrderListItems } from "../models/order.model";
+import { fetchOrderDetail, fetchOrders } from "../api/order.api";
 
 export const useOrders = () => {
-    const [orders, setOrders] = useState<Order[]>([]);
-    
+    const [orders, setOrders] = useState<OrderListItems[]>([]);
+    const [selectedItemId, setSelectedItemId] = useState<number | null>(null);
+
+    console.log(orders);
+
     useEffect(() => {
         fetchOrders().then((orders) => {
             setOrders(orders);
         })
     }, []);
 
-    return { orders }
+    const selectOrderItem = (orderId: number) => {
+        // 요청 방어
+        if (orders.filter((item) => item.order_id === orderId)[0].detail) {
+            setSelectedItemId(orderId);
+            return;
+        }
+
+        fetchOrderDetail(orderId).then((orderDetail) => {
+            setSelectedItemId(orderId);
+            setOrders(
+                orders.map((item) => {
+                    if (item.order_id === orderId) {
+                        return {
+                            ...item,
+                            detail: orderDetail
+                        }
+                    }
+                    return item;
+                })
+            )
+        });
+    }
+
+    return { orders,  selectedItemId, selectOrderItem };
 }
