0aa988d123395e0510570ad5a2034acd4d3f1581
ssoferret <owosso1212@gmail.com>
2024-06-30 14:27:53 +0900

    [Feat/#13] 주문 내역 조회 페이지 주문 내역 표 작성

--- bookstore/src/pages/OrderList.tsx
+++ bookstore/src/pages/OrderList.tsx
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 import Title from '../components/common/Title';
+import { useOrders } from '../hooks/useOrders';
+import { formatDate } from '../utils/format';
 
 function OrderList () {
+    const { orders } = useOrders();
+    console.log(orders);
+    console.log(typeof orders);
     return (
         <>
             <Title size='large'>주문 내역</Title>
@@ -10,7 +15,7 @@ function OrderList () {
                     <thead>
                         <tr>
                             <th>id</th>
-                            <th>주문일사</th>
+                            <th>주문일자</th>
                             <th>주소</th>
                             <th>수령인</th>
                             <th>전화번호</th>
@@ -20,6 +25,20 @@ function OrderList () {
                         </tr>
                     </thead>
                     <tbody>
+                        {
+                            orders.map((order) => (
+                                <tr>
+                                    <td>{order.order_id}</td>
+                                    <td>{formatDate(order.created_at)}</td>
+                                    <td>{order.address}</td>
+                                    <td>{order.receiver}</td>
+                                    <td>{order.contact}</td>
+                                    <td>{order.book_title}</td>
+                                    <td>{order.total_quantity}</td>
+                                    <td>{order.total_price}</td>
+                                    </tr>
+                            ))
+                        }
                         <tr></tr>
                     </tbody>
                 </table>
