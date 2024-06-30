60c1ff1d0d3e1da5dace7a1c702e4880e6cfb506
ssoferret <owosso1212@gmail.com>
2024-06-30 15:47:11 +0900

    [Feat/#13] 주문 내역 상세 조회 버튼 이벤트 및 리스트 출력 작성

--- bookstore/src/pages/OrderList.tsx
+++ bookstore/src/pages/OrderList.tsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 import Title from '../components/common/Title';
 import { useOrders } from '../hooks/useOrders';
-import { formatDate } from '../utils/format';
+import { formatDate, formatNumber } from '../utils/format';
+import Button from '../components/common/Button';
 
 function OrderList () {
-    const { orders } = useOrders();
-    console.log(orders);
-    console.log(typeof orders);
+    const { orders, selectedItemId, selectOrderItem } = useOrders();
+
     return (
         <>
             <Title size='large'>주문 내역</Title>
@@ -22,21 +22,50 @@ function OrderList () {
                             <th>대표상품명</th>
                             <th>수량</th>
                             <th>금액</th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             orders.map((order) => (
+                                <>
                                 <tr>
                                     <td>{order.order_id}</td>
-                                    <td>{formatDate(order.created_at)}</td>
+                                    <td>{formatDate(order.created_at, "YYYY.MM.DD")}</td>
                                     <td>{order.address}</td>
                                     <td>{order.receiver}</td>
                                     <td>{order.contact}</td>
                                     <td>{order.book_title}</td>
-                                    <td>{order.total_quantity}</td>
-                                    <td>{order.total_price}</td>
+                                    <td>{order.total_quantity} 권</td>
+                                    <td>{formatNumber(order.total_price)} 원</td>
+                                    <td>
+                                        <Button size="small" scheme='normal' onClick={() => {selectOrderItem(order.order_id)}}>
+                                            자세히
+                                        </Button>
+                                    </td>
                                     </tr>
+                                    {
+                                        selectedItemId === order.order_id && (
+                                            <tr>
+                                                <td></td>
+                                                <td colSpan={8}>
+                                                    {
+                                                        order.detail &&
+                                                        order.detail.map((item) => (
+                                                            <li key={item.book_id}>
+                                                                <div>
+                                                                    <span>{item.book_id}</span>
+                                                                    <span>{item.author}</span>
+                                                                    <span>{formatNumber(item.price)}원</span>
+                                                                </div>
+                                                            </li>
+                                                        ))
+                                                    }
+                                                </td>
+                                            </tr>
+                                        )
+                                    }
+                                </>
                             ))
                         }
                         <tr></tr>
@@ -49,7 +78,23 @@ function OrderList () {
 }
 
 const OrderListStyle = styled.div`
+    padding: 24px 0 0 0;
 
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        border-top: 1px solid ${({theme}) => theme.color.border};
+        border-bottom: 1px solid ${({theme}) => theme.color.border};
+        font-size: 14px;
+        
+        th, 
+        td {
+            padding: 16px;
+            border-bottom: 1px solid ${({theme}) => theme.color.border};
+            text-align: center;
+            min-width: 90px;
+        }
+    }
 `;
 
 export default OrderList;
