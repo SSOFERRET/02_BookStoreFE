b1a70ede0f81dc604daf3387fb42618a9ae289a1
ssoferret <owosso1212@gmail.com>
2024-07-01 16:12:15 +0900

    [Feat/#16] 상세 도서 조회 페이지에 리뷰 컴포넌트 배치

--- bookstore/src/pages/BookDetail.tsx
+++ bookstore/src/pages/BookDetail.tsx
@@ -9,6 +9,7 @@ import { Link } from "react-router-dom";
 import EllipsisBox from "@/components/common/EllipsisBox";
 import LikeButton from "@/components/book/LikeButton";
 import AddToCart from "@/components/book/AddToCart";
+import BookReview from "@/components/book/BookReview";
 
 const bookInfoList = [
     {
@@ -50,7 +51,7 @@ const bookInfoList = [
 
 function BookDetail() {
     const { bookId } = useParams();
-    const { book, likeToggle } = useBookDetail(bookId);
+    const { book, likeToggle, reviews } = useBookDetail(bookId);
 
     if (!book) return null;
 
@@ -88,6 +89,8 @@ function BookDetail() {
                 <EllipsisBox linelimit={4}>{book.detail}</EllipsisBox>
                 <Title size="medium">목차</Title>
                 <p className="contents">{book.contents}</p>
+                <Title size="medium">리뷰</Title>
+                <BookReview reviews={reviews} />
             </div>
         </BookDetailStyle>
     )
