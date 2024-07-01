2e5b07444807375b31855ea78f9e5e7eedb62fe1
ssoferret <owosso1212@gmail.com>
2024-07-01 17:07:51 +0900

    [Feat/#17] 리뷰 작성 모킹 서버 API 작성

--- bookstore/src/mock/review.ts
+++ bookstore/src/mock/review.ts
@@ -11,7 +11,21 @@ const mockReviewData: BookReviewItem[] = Array.from({length: 8}).map((_, index) 
 }))
 
 export const reviewsById = http.get("http://localhost:9999/reviews/:bookId", () => {
-    return HttpResponse.json(mockReviewData, {
-        status: 200,
-    })
+    return HttpResponse.json(
+        mockReviewData,
+        {
+            status: 200,
+        }
+    )
+})
+
+export const addReview = http.post("http://localhost:9999/reviews/:bookId", () =>{
+    return HttpResponse.json(
+        {
+            message: "리뷰가 등록되었습니다.",
+        },
+        {
+            status: 2001
+        }
+    )
 })
